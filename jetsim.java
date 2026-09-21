da0f9142e6202943afdf9872b55fd8312644a3dc
Akaran <akaran.thileepan@student.rjuhsd.us>
2026-09-21 12:17:08 -0700

    Update jetsim.java

--- jetsim.java
+++ jetsim.java
@@ -25,11 +25,19 @@ public void displayInfo() {
 
 public static void randomEvent(){
    int event = (int)(Math.random() * 100)+1; // Generates a random number between 1 and 100
-    if(event<=5)
+    if(event<=10){
+      playerJet.enemyAircraft();
+    }
+    else if(event<=25){
+      playerJet.unknownAircraft();
+    }
+    else{
+      System.out.print("No events occurred during this part of the flight.");
+    }
 }
 
 public void unknownAircraft(){
-     
+    Scanner scanner = new Scanner(System.in);
     System.out.print("Unknown aircraft spotted. Will you investigate or continue on your flight?");
   if("investigate".equals(scanner.nextLine()) && (Math.random()*2+1) == 1){
     System.out.print("You identify it as an enemy aircraft.");
@@ -40,11 +48,31 @@ public void unknownAircraft(){
     System.out.print("You identify it as an ally and continue on your flight.");
   }
 }
+public void enemyAircraft(){
+    System.out.print("Enemy aircraft spotted. Will you engage or evade?");
+  Scanner scanner = new Scanner(System.in);
+  if("engage".equals(scanner.nextLine())){
+    System.out.print("You engage the enemy aircraft successfully.");
+       if(numMissiles>0){
+           numMissiles--;
+       }
+       else if(numMissiles==0 && (Math.random()*2+1) == 1){
+           System.out.print("You win in a dogfight.");
+       }
+       else{
+            System.out.print("You lose in a dogfight.");
+       }
+  }
+  else if("evade".equals(scanner.nextLine()) && (Math.random()*2+1) == 2){
+    System.out.print("You evade the enemy aircraft.");
+  }
+}
 public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     jetsim playerJet = new jetsim("F-22 Raptor", 6, 2, 2410, 8200);
     playerJet.displayInfo();
     randomEvent();
+    
     unknownAircraft();
 }
 
